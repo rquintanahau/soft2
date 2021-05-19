e43c5d3dd0b635f7f4efa35e5e03b2016ac096a6
Rodrigo Quintana <reqh1095@gmail.com>
2021-05-19 18:16:04 -0500

    v1.1

--- public/assets/js/perfil.js
+++ public/assets/js/perfil.js
@@ -1,7 +1,9 @@
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
+const editLinks = document.querySelectorAll('.editf');
 
 const setupUI=(user) => {
+  editLinks.forEach(item => item.style.display = 'none');
   if(user){
     loggedInLinks.forEach(item => item.style.display = 'block');
     loggedOutLinks.forEach(item => item.style.display = 'none');
@@ -27,13 +29,31 @@ firebase.auth().onAuthStateChanged(function(user) {
       nombreinfo.innerHTML = name;
       bioinfo.innerHTML = bio;
     })
-
-
-
-
   } else {
     userinfo.innerHTML = '';
     console.log("0");
     setupUI();
+    editLinks.forEach(item => item.style.display = 'none');
   }
 });
+
+
+
+const setupEdit =document.querySelector('.editbutton');
+setupEdit.addEventListener('click', (e) => {
+  editLinks.forEach(item => item.style.display = 'block');
+  });
+
+
+const editform = document.querySelector('#edit-form');
+editform.addEventListener('submit', (e) => {
+  e.preventDefault();
+  user = firebase.auth().currentUser;
+  db.collection('users').doc(user.uid).update({
+    nombre: editform['edit-nombre'].value,
+    bio: editform['edit-bio'].value
+  }).then(() =>{
+    editform.reset();
+    window.location.reload();
+  });
+  });
